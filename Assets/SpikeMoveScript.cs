9639bfc326184547e4b9808c421846403213a1c5
Michael Gauthier <mikegauthier222@hotmail.com>
2025-08-06 15:02:49 -0400

    testing console logs

--- Assets/SpikeMoveScript.cs
+++ Assets/SpikeMoveScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class SpikeMoveScript : MonoBehaviour
@@ -8,14 +9,15 @@ public class SpikeMoveScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("this is a test");
+        print("hi");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
         
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
     }
 }
